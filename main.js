4282bb7223dedb0786abe587a722cf67162a594b
25vudinhvinh <154252385+25vudinhvinh@users.noreply.github.com>
2024-10-29 11:46:11 +0700

    xu li phan radio va lay gia tri radio khi submit

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const Validator = (options) =>{
     const formElement = document.querySelector(options.form)
     const selectorRules = {}
+
+    // ham validate
     const validate = (inputElement, rule)=>{
         const errorElement = getParent(inputElement, options.parentInput).querySelector('.form-message')
         const rules = selectorRules[rule.selector]
         let errorMessage 
         for (var i = 0; i < rules.length; ++i){
-           errorMessage = rules[i](inputElement.value)
+            switch (inputElement.type){
+                case 'checkbox':
+                case 'radio':
+                    // doan nay khong hieu 
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector + ':checked')
+                    )
+                    break
+                default:
+                    errorMessage = rules[i](inputElement.value)
+            }
            if(errorMessage) break
         }
         if(errorMessage){
@@ -36,6 +48,7 @@ const Validator = (options) =>{
 
             let isFormValid = true
 
+            // lap qua tung rules de validate
             options.rules.forEach((rule) =>{
             const inputElement = formElement.querySelector(rule.selector)
               const isValid = validate(inputElement, rule)
@@ -45,16 +58,25 @@ const Validator = (options) =>{
             })
 
             if(isFormValid){
-                if (typeof options.onSubmit === 'function'){
+                if (typeof options.onSubmit === 'function'){ 
                     const enableInput = formElement.querySelectorAll('[name]')
                     const formValues = Array.from(enableInput).reduce((values, input) =>{
-                        values[input.name] = input.value
+                        switch(input.type){
+                            case 'checkbox':
+                            case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="'+input.name+'"]:checked').value
+                                break
+                            default:
+                                values[input.name] = input.value
+                        }
                         return values
                     }, {})
                     options.onSubmit(formValues)
                 }
             }
         }
+
+        // push ham rule.test vao thanh mot mang selectorRules
         options.rules.forEach(rule =>{
             if(Array.isArray(selectorRules[rule.selector])){
                 selectorRules[rule.selector].push(rule.test)
@@ -62,17 +84,20 @@ const Validator = (options) =>{
                 selectorRules[rule.selector] = [rule.test]
             }
 
-            const inputElement = formElement.querySelector(rule.selector)
-            if (inputElement){
-                const errorElement = getParent(inputElement, options.parentInput).querySelector('.form-message')
-                inputElement.onblur = ()=>{
-                    validate(inputElement, rule)
+            // lay ra tat ca cac the inputElement
+            const inputElements = formElement.querySelectorAll(rule.selector)
+            inputElements.forEach((inputElement)=>{
+                if (inputElement){
+                    const errorElement = getParent(inputElement, options.parentInput).querySelector('.form-message')
+                    inputElement.onblur = ()=>{
+                        validate(inputElement, rule)
+                    }
+                    inputElement.oninput = ()=>{
+                        errorElement.innerText = ''
+                        getParent(inputElement, options.parentInput).classList.remove('invalid')
+                    }   
                 }
-                inputElement.oninput = ()=>{
-                    errorElement.innerText = ''
-                    getParent(inputElement, options.parentInput).classList.remove('invalid')
-                }   
-            }
+            })
         })
     }
 }
